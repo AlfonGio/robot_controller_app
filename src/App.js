7e01d6b9e7ad15456ee62282a30791084e644ccc
Orin <alfonsus.chrisanto@gmail.com>
2023-12-15 10:21:27 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleButtonClick = () => {
+    fetch('http://localhost:3002/run-script', { method: 'POST' })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Network response was not ok, status: ${response.status}`);
+      }
+      return response.text();
+    })
+    .then(text => {
+      try {
+        const jsonData = JSON.parse(text);
+        console.log('JSON data:', jsonData);
+        return jsonData;
+      } catch (error) {
+        console.error('Failed to parse JSON:', text);
+        throw error;
+      }
+    })
+    .then(data => {
+      console.log('Script executed successfully:', data);
+    })
+    .catch(error => console.error('Error:', error));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +42,9 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={handleButtonClick}>
+          Run Script
+        </button>
       </header>
     </div>
   );
